7f44b7d736af9796716db6a0b4652a9798b47c7a
sanah saleem <sanahsaleem2000@gmail.com>
2025-11-22 04:11:24 +0400

    change for deployment

--- frontend/src/api/nakamaClient.ts
+++ frontend/src/api/nakamaClient.ts
@@ -1,8 +1,8 @@
 import { Client } from "@heroiclabs/nakama-js";
 
-const useSSL = false; // true when we deploy with HTTPS
-const host = "127.0.0.1"; // or "localhost"
-const port = "7350";
-const serverKey = "tictactoe-server-key"; // from backend/local.yml
+const useSSL = import.meta.env.VITE_NAKAMA_USE_SSL === "true"
+const host = import.meta.env.VITE_NAKAMA_HOST! //"127.0.0.1";
+const port = import.meta.env.VITE_NAKAMA_PORT || "7350";
+const serverKey = import.meta.env.VITE_NAKAMA_SERVER_KEY || "tictactoe-server-key";
 
 export const nakamaClient = new Client(serverKey, host, port, useSSL);
